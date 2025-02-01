c87c4b481de7acd8faec9708d6da234dbf8f3fce
Aoichaan0513 <aoichaan0513@gmail.com>
2025-02-01 13:03:56 +0900

    [Update] コンポーネントの改善

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export * from './components';
+export * from './interfaces';
+export * from './transformers';
 export * from './utils';
--- src/transformers/index.ts
+++ src/transformers/index.ts
@@ -1,2 +1,11 @@
+import { Content } from '../interfaces';
+import { contentToMarkdown } from './markdown';
+import { contentToReactNode } from './react';
+
+export const ContentTransformer = {
+    toReact: (content: Content) => contentToReactNode(content),
+    toMarkdown: (content: Content) => contentToMarkdown(content)
+};
+
 export * from './markdown';
 export * from './react';
