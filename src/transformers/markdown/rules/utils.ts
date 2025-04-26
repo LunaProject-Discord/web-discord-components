a74bb97e9579d7a082104e35a906a8ee482bf59a
Aoichaan0513 <aoichaan0513@gmail.com>
2025-04-26 14:44:15 +0900

    [Update] Markdown パース時の初期ステータスを変更

--- src/transformers/markdown/rules/utils.ts
+++ src/transformers/markdown/rules/utils.ts
@@ -64,7 +64,7 @@ export const createMarkdownParser = (rules: Rule[]) => {
             completed: '',
             inQuote: false,
             listDepth: 0,
-            parseParagraphs: false
+            parseParagraphs: true
         }
     );
 };
